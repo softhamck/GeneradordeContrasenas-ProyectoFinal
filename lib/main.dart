5c4476e7e09930d012fb1d0682e309a22b242273
softhamck <andreamunozcano5@gmail.com>
2023-10-29 18:35:00 -0500

    Modificación Archivo .dart, con la estructura del proyecto

--- lib/main.dart
+++ lib/main.dart
@@ -1,114 +1,152 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'Generador de Contraseñas',
       theme: ThemeData(
-        colorSchemeSeed: Colors.indigo,
-        useMaterial3: true,
-        brightness: Brightness.light,
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple),
       ),
-      darkTheme: ThemeData(
-        colorSchemeSeed: Colors.blue,
-        useMaterial3: true,
-        brightness: Brightness.dark,
-      ),
-      home: const MyHomePage(title: 'Flutter Example App'),
-      debugShowCheckedModeBanner: false,
+      home: PasswordGenerator(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class PasswordGenerator extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _PasswordGeneratorState createState() => _PasswordGeneratorState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _PasswordGeneratorState extends State<PasswordGenerator> {
+  String generatedPassword = '';
+  final TextEditingController lengthController = TextEditingController();
+  bool useUppercase = true;
+  bool useLowercase = true;
+  bool useNumbers = true;
+  bool useSpecialChars = true;
+  String errorText = '';
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  String generatePassword() {
+    final String uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    final String lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    final String numberChars = '0123456789';
+    final String specialChars = '!@#\$%^&*()_+[]{}|;:,.<>?';
+
+    String allowedChars = '';
+    if (useUppercase) allowedChars += uppercaseChars;
+    if (useLowercase) allowedChars += lowercaseChars;
+    if (useNumbers) allowedChars += numberChars;
+    if (useSpecialChars) allowedChars += specialChars;
+
+    if (allowedChars.isEmpty) {
+      return 'Selecciona al menos un tipo de carácter.';
+    }
+
+    final random = Random();
+    String password = '';
+    for (int i = 0; i < int.parse(lengthController.text); i++) {
+      password += allowedChars[random.nextInt(allowedChars.length)];
+    }
+    return password;
+  }
+
+  void generateButtonPressed() {
+    if (lengthController.text.isEmpty) {
+      setState(() {
+        errorText = 'Por favor, ingresa la longitud de la contraseña.';
+      });
+    } else {
+      setState(() {
+        errorText = '';
+        generatedPassword = generatePassword();
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Generador de Contraseñas'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            TextField(
+              controller: lengthController,
+              keyboardType: TextInputType.number,
+              decoration:
+                  InputDecoration(labelText: 'Longitud de la contraseña'),
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              errorText,
+              style: TextStyle(color: Colors.red),
+            ),
+            SizedBox(height: 10),
+            CheckboxListTile(
+              title: Text('Mayúsculas'),
+              value: useUppercase,
+              onChanged: (value) {
+                setState(() {
+                  useUppercase = value!;
+                });
+              },
+            ),
+            CheckboxListTile(
+              title: Text('Minúsculas'),
+              value: useLowercase,
+              onChanged: (value) {
+                setState(() {
+                  useLowercase = value!;
+                });
+              },
+            ),
+            CheckboxListTile(
+              title: Text('Números'),
+              value: useNumbers,
+              onChanged: (value) {
+                setState(() {
+                  useNumbers = value!;
+                });
+              },
+            ),
+            CheckboxListTile(
+              title: Text('Caracteres especiales'),
+              value: useSpecialChars,
+              onChanged: (value) {
+                setState(() {
+                  useSpecialChars = value!;
+                });
+              },
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: generateButtonPressed,
+              child: Text('Generar Contraseña'),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Contraseña generada:',
+              style: TextStyle(
+                fontSize: 20, // Tamaño de la fuente
+                fontWeight: FontWeight.bold, // Negrita
+              ),
+            ),
+            Text(
+              generatedPassword,
+              style: TextStyle(
+                fontSize: 20, // Tamaño de la fuente
+                fontWeight: FontWeight.bold, // Negrita
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
